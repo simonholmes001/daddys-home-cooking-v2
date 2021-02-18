d888483b63a8120c83c3ad9952eca45584f91f6a
simonholmes001 <simonholmesabc@gmail.com>
2021-02-18 17:08:09 +0100

    Intermediate push

--- src/app/create-recipes/create-recipes/create-recipes.component.ts
+++ src/app/create-recipes/create-recipes/create-recipes.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+
 import { TaskService } from 'src/app/task.service';
 
 @Component({
@@ -8,7 +10,9 @@ import { TaskService } from 'src/app/task.service';
 })
 export class RecipeCreateComponent implements OnInit {
 
-  constructor(private taskService: TaskService) { }
+  public recipes!: any;
+
+  constructor(private taskService: TaskService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
   }
@@ -16,10 +20,23 @@ export class RecipeCreateComponent implements OnInit {
   createRecipe(recipeName: string, difficulty: string, prepTime: string, cookTime: string, ingredients: string,
     step1: string, step2: string, step3: string, step4: string, step5: string, step6: string, step7: string, step8: string, step9: string, step10: string, imgURL: string, videoURL: string, additionalLinks: string, tags: string) {
     this.taskService.createRecipe(recipeName, difficulty, prepTime, cookTime, ingredients, step1, step2, step3, step4, step5, step6, step7, step8, step9, step10, imgURL, videoURL, additionalLinks, tags).subscribe((response: any) => {
-      window.location.replace('create-recipes');
+      // window.location.replace('create-recipes');
+      // ADDED IN THIS CODE TO TRY AND GET THE INDIVIDUAL RECIPE FROM THE POST API CALL FROM createRecipe => NOT WORKING
+      // LOOK INTO PROMISES TO SOLVE THIIS PROBLEM
+      this.route.params.subscribe(
+        (params: Params) => {
+          this.taskService.getIndividualRecipes(params.id).subscribe((recipes: any) => {
+            return this.recipes = recipes;
+        });
+        }
+        )
       console.log(response)
     });
   }
+
+  sendNotification() {
+
+  };
 
 }
 
